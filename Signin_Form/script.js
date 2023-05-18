219d839b25e69b23ba9a5fdb1cfe9cdb01ed49f0
Sirius <pranav474738@gmail.com>
2023-05-18 18:25:31 +0530

    Added validation on keystroke

--- Signin_Form/script.js
+++ Signin_Form/script.js
@@ -1,77 +1,72 @@
-// Validation Part
-
-// let count = 0;
-// let error_string = "";
-
-// function reg_check_email() {
-//     let reg_email = //
-//     let email = document.getElementsByTagName("input")[0].value;
-//     if(reg_email.test(email)){
-//        count++;
-//     }
-//     else{
-//         error_string += "Invalid email "
-//     }
-// }
-
-
-// function master_check () {
-    
-    
-    
-//     reg_check_email();
-//     if(count != 2){
-//         alert(error_string);
-//     }
-// }
-
 //API Post Part
 
 const post = (() => {
-    let regex_email = /^[a-zA-Z0-9]+((@[a-zA-Z0-9]+.[a-z]+)|.[a-z]+@[a-zA-Z0-9]+.[a-z]+$)/
     const email = document.getElementById("email").value;
-    if(!regex_email.test(email)){
-        msg_container.textContent = "The email is invalid. It should be of the form abc@gmail.com or abc.def@abc.bc";
+    const pass = document.getElementById("pass-1").value;
+    let msg_container = document.getElementById("validation");
+    if(flag == 1){
+        let body = {
+            email,
+            pass
+        };
+        console.log(JSON.stringify(body));
+        fetch("https://reqres.in/api/login" , {
+        method : "POST",
+        body: JSON.stringify({email : email, password : pass}),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8"
+          }
+    })
+        .then(function (response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+            if(data.error != undefined){
+                msg_container.textContent = data.error;
+            }
+            else{
+                msg_container.textContent = "Login Succesfull";
+            }
+            
+        })
     }
     else{
-        let strong_password = /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9]).{8,}$/;
-        const pass = document.getElementById("pass-1").value;
-        if(!strong_password.test(pass)){
-            msg_container.textContent = "That's not a strong password. A strong password contains atleast one uppercase letter , atleast one symbol , atleast one digit ,minimum length 8";
-        }
-        else{
-            console.log(email);
-            console.log(pass);
-            let body = {
-                email,
-                pass
-            };
-            console.log(JSON.stringify(body));
-            fetch("https://reqres.in/api/login" , {
-            method : "POST",
-            body: JSON.stringify({email : email, password : pass}),
-              headers: {
-                "Content-type": "application/json; charset=UTF-8"
-              }
-        })
-            .then(function (response){
-                return response.json();
-            })
-            .then(function(data){
-                console.log(data);
-                if(data.error != undefined){
-                    msg_container.textContent = data.error;
-                }
-                else{
-                    msg_container.textContent = "Login Succesfull";
-                }
-                
-            })
-        }
+        alert("You need to enter the required details before logging in!");
+    }
+    
     
     }   
     
-})
+)
 
-let msg_container = document.getElementById("validation");
+let msg_container_email = document.getElementById("validation-email");
+let msg_container_password = document.getElementById("validation-password");
 
+// Validation part
+let flag = 0;
+function reg_check_email () {
+    let regex_email = /^^[a-zA-Z0-9]+((@[a-z]{4,}\.[a-z]{3})|(\.[a-z]{2,}@reqres.in))$/
+    const email = document.getElementById("email").value;
+    if(regex_email.test(email)){
+        msg_container_email.textContent = " ";
+        flag = 1;
+    }
+    else{
+        msg_container_email.textContent = "The email is invalid. It should be of the form abc@gmail.com or abc.def@reqres.in";
+        flag = 0;
+    }
+}
+
+function reg_check_password () {
+    let strong_password = /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9]).{8,}$/;
+    const pass = document.getElementById("pass-1").value;
+    if(strong_password.test(pass)){
+        msg_container_password.textContent = " ";
+        flag = 1;
+    }
+    else{
+        msg_container_password.textContent = "That's not a strong password. A strong password contains atleast one uppercase letter , atleast one symbol , atleast one digit ,minimum length 8";
+        flag = 0;
+    }
+}
